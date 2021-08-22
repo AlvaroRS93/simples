2030ed038e628a4e986c731be8ac6eda701b213f
AlvaroRS93 <86481086+AlvaroRS93@users.noreply.github.com>
2021-08-22 14:42:15 -0300

    Add files via upload

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="d-flex justify-center">
+      <v-img
+        class="ma-3"
+        max-height="150"
+        max-width="250"
+        src="../assets/logo.png"
+      ></v-img>
+    </div>
+
+    <div id="nav" class="d-flex justify-center">
+      <v-app-bar
+        class="d-flex justify-center primary"
+        elevation="3"
+        max-width="1200"
+      >
+        <v-btn class="primary black--text" depressed @click="mudarTela('/')"
+          >Principal</v-btn
+        >
+        <v-btn
+          class="primary black--text"
+          depressed
+          @click="mudarTela('/contato')"
+          >Contato</v-btn
+        >
+        <v-btn
+          class="primary black--text"
+          depressed
+          @click="mudarTela('/quem-somos')"
+          >Quem Somos</v-btn
+        >
+        <v-btn
+          class="primary black--text"
+          depressed
+          @click="mudarTela('/servicos')"
+          >Serviços</v-btn
+        >
+        <v-col cols="2" md="4" lg="8">
+          <v-icon large right class="d-flex justify-end">mdi-instagram</v-icon>
+        </v-col>
+      </v-app-bar>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    mudarTela: function (tela) {
+      this.$router.push(tela);
+    },
+  },
+};
+</script>
---
+++ src/components/carroussel.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-col>
+    <div>
+      <v-card elevation="10">
+        <v-col>
+          <v-carousel cycle height="300" show-arrows-on-hover>
+            <v-carousel-item
+              v-for="(img, i) in imagens"
+              :key="i"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+            >
+              <v-img :src="img.src" contain max-height="350"></v-img>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+      </v-card>
+    </div>
+  </v-col>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      imagens: [
+        { src: require("../assets/slides/01.jpeg") },
+        { src: require("../assets/slides/02.jpeg") },
+        { src: require("../assets/slides/03.jpeg") },
+        { src: require("../assets/slides/04.jpeg") },
+      ],
+    };
+  },
+};
+</script>
