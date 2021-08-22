05fdbcdbbb079d58d72cafd6332b5445d298dc7f
AlvaroRS93 <86481086+AlvaroRS93@users.noreply.github.com>
2021-08-22 14:42:42 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,57 @@
 <template>
   <v-app id="fundo">
     <v-row>
-    <v-col cols="2" class="gren">
+      <v-col cols="2" class="gren"> </v-col>
+      <v-col cols="8">
+        <cabecalho></cabecalho>
+        <div id="app" class="">
+          <router-view class="" />
 
-    </v-col>
-    <v-col cols="8">
-    <div class="d-flex justify-center">
-      <v-img
-        class="ma-3"
-        max-height="150"
-        max-width="250"
-        src="./assets/logo.png"
-      ></v-img>
-    </div>
-    <div id="app" class="">
-      <div id="nav" class="d-flex justify-center">
-        <v-app-bar
-          class="d-flex justify-center primary"
-          elevation="3"
-          max-width="1200"
-        >
-          <v-btn class="primary black--text" depressed @click="mudarTela('/')"
-            >Principal</v-btn
-          >
-          <v-btn
-            class="primary black--text"
-            depressed
-            @click="mudarTela('/contato')"
-            >Contato</v-btn
-          >
-          <v-btn
-            class="primary black--text"
-            depressed
-            @click="mudarTela('/quem-somos')"
-            >Quem Somos</v-btn
-          >
-          <v-btn
-            class="primary black--text"
-            depressed
-            @click="mudarTela('/servicos')"
-            >Serviços</v-btn
-          >
-          <v-col cols="2" md="4" lg="8">
-            <v-icon large right class="d-flex justify-end"
-              >mdi-instagram</v-icon
+          <v-footer fixed dark id="rodape" class="mb-5 d-flex justify-center">
+            <v-card
+              width="1200"
+              max-height="100"
+              elevation="10"
+              class="primary black--text text-center"
             >
-          </v-col>
-        </v-app-bar>
-      </div>
-      <router-view class="" />
+              <v-card-text class="mt-n2">
+                <v-btn
+                  class="mx-2 black--text"
+                  icon
+                  href="https://www.instagram.com/alvarors_93/"
+                  target="_blank"
+                >
+                  <v-icon size="30px"> mdi-instagram </v-icon>
+                </v-btn>
+              </v-card-text>
 
-      <v-footer  fixed  dark id="rodape" class="mb-5 d-flex justify-center" >
-        <v-card width="1200" max-height="100" elevation="2" class="primary black--text text-center">
-          <v-card-text class="mt-n2">
-            <v-btn
-              class="mx-2 black--text"
-              icon
-              href="https://www.instagram.com/alvarors_93/"
-              target="_blank"
-            >
-              <v-icon size="30px">
-                mdi-instagram 
-              </v-icon>
-            </v-btn>
-          </v-card-text>
+              <v-divider class="mx-5 mt-n2 grey"></v-divider>
 
-          <v-divider class="mx-5 mt-n2 grey" ></v-divider>
+              <v-card-text class="mt-n2 black--text">
+                {{ new Date().getFullYear() }} —
+                <strong>Desenvolvido por Alvaro Rodrigues</strong>
+              </v-card-text>
+            </v-card>
+          </v-footer>
+        </div>
+      </v-col>
 
-          <v-card-text class="mt-n2 black--text">
-            {{ new Date().getFullYear() }} — <strong>Desenvolvido por Alvaro Rodrigues</strong>
-          </v-card-text>
-        </v-card>
-      </v-footer>
-    </div> 
-    </v-col>
-
-    <v-col cols="2" class="gren">
-
-    </v-col>
+      <v-col class="" cols="2" align-self="center">
+        <div class="d-flex justify-center">
+          <carroussel></carroussel>
+        </div>
+      </v-col>
     </v-row>
   </v-app>
-  
 </template>
 <script>
+import Carroussel from "./components/carroussel.vue";
+import Cabecalho from "./components/Cabecalho.vue";
+
 export default {
-  methods: {
-    mudarTela: function (tela) {
-      this.$router.push(tela);
-    },
+  components: {
+    carroussel: Carroussel,
+    cabecalho: Cabecalho,
   },
 };
 </script>
@@ -101,7 +65,8 @@ export default {
   color: #2c3e50;
 }
 
-#fundo, #rodape {
+#fundo,
+#rodape {
   background-color: #fefae0;
 }
 
