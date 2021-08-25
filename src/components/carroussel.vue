ddd725f30293e3502bbe0cd7508ada3f65dfa34e
AlvaroRS93 <86481086+AlvaroRS93@users.noreply.github.com>
2021-08-24 21:29:31 -0300

    Add files via upload

--- src/components/carroussel.vue
+++ src/components/carroussel.vue
@@ -1,22 +1,34 @@
 <template>
-
-    <div class=" ma-3 d-flex justify-center">
-      <v-card width="600" height="" elevation="10">
-        <v-col>
-          <v-carousel cycle height="300" show-arrows-on-hover>
-            <v-carousel-item
-              v-for="(img, i) in imagens"
-              :key="i"
-              reverse-transition="fade-transition"
-              transition="fade-transition"
-            >
-              <v-img :src="img.src" contain max-width="500"></v-img>
-            </v-carousel-item>
-          </v-carousel>
-        </v-col>
-      </v-card>
-    </div>
-  
+  <div class="ma-3 d-flex justify-start">
+    <v-card class="primary" width="285" height="" elevation="10">
+      <v-card-text class="pb-0 font-weight-bold black--text text-h6" style="wordword-break: normal !important" >
+        Também estamos no Instagram!        
+      </v-card-text>
+      <v-col>
+        <v-carousel cycle height="250" show-arrows-on-hover hide-delimiters>
+          <v-carousel-item
+            v-for="(img, i) in imagens"
+            :key="i"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+            <v-img :src="img.src" contain max-width="500"></v-img>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+      <div class="d-flex justify-center mb-2">
+        <v-btn
+          elevation="5"
+          fab
+          icon
+          target="_blank"
+          href="https://www.instagram.com/simples_solucoesfinanceiras"
+        >
+          <v-icon class="black--text" large>mdi-instagram</v-icon>
+        </v-btn>
+      </div>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
